ace657b1dc5a48bfbd2b3cfe884a6b324a4576e5
Jack Remmert <61426053+jremmert@users.noreply.github.com>
2020-07-30 09:38:28 -0400

    Create Service Time Webscrape Function

---
+++ FanGraphs_ServiceTime.py
@@ -0,0 +1,44 @@
+import bs4 as bs
+import urllib.request
+
+def get_service_time(name,f_id):
+
+    # remove any periods such as those in Jr. or Sr.
+    name = name.replace(".","")
+    # remove any spaces and replace with dashes
+    name = name.replace(" ","-")
+    # convert to lowercase
+    name = name.lower()
+    
+    # url
+    address=("https://www.fangraphs.com/players/"+name+"/"+str(f_id)+"/stats")
+    # submit and read url
+    source = urllib.request.urlopen(address).read()      
+    soup = bs.BeautifulSoup(source,"html")
+    
+    # find tag for service time
+    svt_html = soup.find_all('div', class_='tool-tip-item')[0]
+    # split on the period since svt_html returns 2 sentences
+    svt_html = svt_html.text.split(".")
+    # remove excess values. First remove the Reported...
+    svt_html.remove("Reported as the pre-2020 value")
+    # then remove the excess space
+    svt_html.remove('')
+    # check to see if working with the string X years and Y days of service time
+    if len(svt_html) !=1:
+        err = "Error with "+str(f_id)+" , Name: "+name
+        return err
+    else:
+        # force back into a string
+        svt_html = svt_html[0]
+        # extract the integers
+        ints = [int(s) for s in svt_html.split() if s.isdigit()]
+        # we know the first value is the year and the second is number of days
+        service_time = ints[0] + ints[1]/365.25
+        return service_time
+
+# name = "Manny Machado"
+# f_id =  11493       
+# get_service_time(name,f_id)
+    
+
